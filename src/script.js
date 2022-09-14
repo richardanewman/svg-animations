298cd0c7debb19482584e1cc18deadb2f7f35b6a
Rick Newman <rick@richardnewman.dev>
2022-09-14 09:42:27 -0400

    line animation

--- src/script.js
+++ src/script.js
@@ -6,6 +6,5 @@ const circle = document.getElementById("circle");
 const circleTotal = circle.getTotalLength();
 console.log('circle: ', circleTotal);
 
-
 const text = document.getElementsByClassName("text");
 Object.keys(text).map((path, index) => console.log(`text path index ${index} length: ${text[path].getTotalLength()} `));
